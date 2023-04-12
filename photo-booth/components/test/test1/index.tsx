df70f47252101b4bc712a4123d554c60fe5cdfb8
kang seok <sekeipost@naver.com>
2023-04-12 22:41:31 +0900

    [FEAT] setting eslint, prettier, styled-component

---
+++ photo-booth/components/test/test1/index.tsx
@@ -0,0 +1,11 @@
+import styled from 'styled-components';
+
+export default function asds() {
+  return <Wrapper></Wrapper>;
+}
+
+const Wrapper = styled.div`
+  width: 500px;
+  height: 500px;
+  background-color: red;
+`;
