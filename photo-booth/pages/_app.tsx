3790d8e1c4e7c3c9c93b99479b4145a8d310dce4
yeram-lim <yeramyeye@gmail.com>
2023-04-16 17:13:36 +0900

    😺[CHORE] create global layout (#2)

--- photo-booth/pages/_app.tsx
+++ photo-booth/pages/_app.tsx
@@ -1,8 +1,15 @@
 import type { AppProps } from 'next/app';
 import '../styles/globals.css';
+import { GlobalLayout } from '@styles/common/Layout';
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+  return (
+    <>
+      <GlobalLayout>
+        <Component {...pageProps} />
+      </GlobalLayout>
+    </>
+  );
 }
 
 export default MyApp;
