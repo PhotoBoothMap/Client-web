c0e98a9bf8379fd45137502551801499b8bf0ec6
yeram-lim <yeramyeye@gmail.com>
2023-06-06 19:04:08 +0900

    🚚[MOVE] login api, validate api to auth file (#5)

--- photo-booth/repositories/login/auth.ts
+++ photo-booth/repositories/login/auth.ts
@@ -1,5 +1,47 @@
 import { setCookie } from 'cookies-next';
 
-export const setRTCookie = (token: any) => {
-  setCookie('RT', token, { httpOnly: true });
+export const setRefreshTokenCookie = (token: any) => {
+  setCookie('refreshToken', token, { httpOnly: true });
+};
+
+export const setAccessTokenCookie = (token: any) => {
+  setCookie('accessToken', token);
+};
+
+export const kakaoLoginApi = async (code: string) => {
+  const response = await fetch(`${process.env.NEXT_PUBLIC_API_HOST}/auth/kakao/`, {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify({
+      authorizationCode: code,
+    }),
+  }).then((res) => res.json());
+
+  return response;
+};
+
+export const validateApi = async (AT: any) => {
+  const response = await fetch(`${process.env.NEXT_PUBLIC_API_HOST}/members/validate/`, {
+    method: 'PUT',
+    headers: {
+      'Content-Type': 'application/json',
+      Authorization: `Bearer ${AT}`,
+    },
+  }).then((res) => res.json());
+
+  return response;
+};
+
+export const reIssueApi = async (refreshToken: any, accessToken: any) => {
+  const response = await fetch(`${process.env.NEXT_PUBLIC_API_HOST}/auth/reissue/`, {
+    method: 'GET',
+    headers: {
+      'Content-Type': 'application/json',
+      Authorization: `Bearer ${accessToken}`,
+    },
+  }).then((res) => res.json());
+
+  return response;
 };
